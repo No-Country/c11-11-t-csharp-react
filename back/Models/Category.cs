430a48daa23d7097f45cbe4b5da5b28d3714b12d
josias jairo <jairo.josias14prueba.com>
2023-05-16 13:55:57 -0500

    feat:add category,roles class

---
+++ back/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace back.Models;
+
+public class Category
+{
+
+    [Key]
+    public int id { get; set; }
+    [DisplayName("Nombre")]
+    [Required(ErrorMessage ="introduce el nombre")]
+    [Column(TypeName = "varchar(200)")]
+    public string name { get; set; }
+
+    [DisplayName("Descripcion")]
+    [Required(ErrorMessage = "introduce la descripcion")]
+    [Column(TypeName = "varchar(200)")]
+    public string description { get; set; }
+
+    [DisplayName("Tipo")]
+    [Required(ErrorMessage = "introduce el tipo de nombre")]
+    [Column(TypeName = "varchar(200)")]
+    public string type { get; set; }
+
+
+}
---
+++ back/Models/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace back.Models;
+
+public class Roles
+{
+
+    [Key]
+    public int id { get; set; }
+
+    [DisplayName("Nombre")]
+    [Required(ErrorMessage ="introduce el nombre")]
+    [Column(TypeName = "varchar(200)")]
+    public string name { get; set; }
+
+}
